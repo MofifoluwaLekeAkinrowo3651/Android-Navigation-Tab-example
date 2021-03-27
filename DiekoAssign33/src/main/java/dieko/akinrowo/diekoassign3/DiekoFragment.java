405d2367772082ec30108fb10772d0cf39a242e6
Mofifoluwa Dieko Akinrowo <n01343651@humbermail.ca>
2021-03-27 18:02:00 -0400

    working on clear canvas for tab 1

--- DiekoAssign33/src/main/java/dieko/akinrowo/diekoassign3/DiekoFragment.java
+++ DiekoAssign33/src/main/java/dieko/akinrowo/diekoassign3/DiekoFragment.java
@@ -1,59 +1,50 @@
 package dieko.akinrowo.diekoassign3;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
 
 public class DiekoFragment extends Fragment {
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public DiekoFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment DiekoFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static DiekoFragment newInstance(String param1, String param2) {
-//        DiekoFragment fragment = new DiekoFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
+    private Button b;
+    private int startx = 10, starty = 10, endx=300, endy=300;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.diekofragment, container, false);
+
+        b = (Button) v.findViewById(R.id.clear_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearCanvas(v);
+            }
+        });
+
+        return v;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.diekofragment, container, false);
+    public void clearCanvas(View v){
+        Canvas canvas =
+        .drawColor(Color.TRANSPARENT);
+        startx = 0;
+        starty = 0;
+        endx = 0;
+        endy = 0;
     }
 }
