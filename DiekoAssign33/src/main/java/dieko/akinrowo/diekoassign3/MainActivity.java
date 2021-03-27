ce0a5d9c9b42c2ad8b31d39c9e1a273c3b1e4542
Mofifoluwa Dieko Akinrowo <n01343651@humbermail.ca>
2021-03-27 19:16:17 -0400

    Last

--- DiekoAssign33/src/main/java/dieko/akinrowo/diekoassign3/MainActivity.java
+++ DiekoAssign33/src/main/java/dieko/akinrowo/diekoassign3/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +54,27 @@ public class MainActivity extends AppCompatActivity {
 
                 return true;
             };
+
+    @Override
+    public void onBackPressed() {
+        Log.d("CDA", "onBackPressed Called");
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setTitle(R.string.NAME_ID);
+
+        alertDialogBuilder
+                .setMessage("Do you want to exit the app?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> {
+
+                    Toast.makeText(getApplicationContext(), "Bye bye!",
+                            Toast.LENGTH_LONG).show();
+                    System.exit(0);
+                })
+
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel());
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 }
